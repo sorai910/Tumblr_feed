89b50be5425c9aad5cf87a4b595ec630a9e9f970
Xiaoran Liu <liuxs@brandeis.edu>
2024-03-25 22:47:10 -0400

    project 5

---
+++ ios101-project5-tumblr/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Xiaoran Liu on 3/25/24.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    @IBOutlet weak var summaryLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
